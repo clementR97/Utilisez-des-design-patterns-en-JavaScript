fc6f83803cec4b9fbcc6929d2b56a653275dce01
clement Roland <clementroland52@gmail.com>
2025-11-18 13:04:39 +0000

    Refactor movie data handling: replace OldMovie model with new Movie model, update API endpoint, and adjust MovieCard template for dynamic image source.

--- js/models/Movie.js
+++ js/models/Movie.js
@@ -1,3 +1,30 @@
 class Movie {
+constructor(data){
+    this._title = data.title
+    this._synopsis = data.synopsis
+    this._picture = data.picture
+    this._duration = data.duration
+    this._released_in = data.released_in
+}
+get title(){
+    return this._title.fr ?? this._title.en;
+    
 
+}
+get duration(){
+
+    return (Math.floor(this._duration/60)+'h'+(this._duration % 60).toString().padStart(2,'0'))
+}
+get synopsis(){
+    return this._synopsis
+}
+get picture(){
+    return `assets/${this._picture}`
+}
+get released_in(){
+    return this._released_in
+}
+get thumbnails(){
+    return `assets/thumbnails/${this._picture}`
+}
 }
--- js/models/OldMovie.js
+++ js/models/OldMovie.js
@@ -1,3 +1,27 @@
 class OldMovie {
-
+constructor(data){
+   this._duration = data.duration
+   this._released_in = data.released_in
+   this._picture = data.picture
+   this._synopsis = data.synopsis
+   this._title = data.title
+}
+get duration(){
+    return this._duration
+}
+get released_in(){
+    return this._released_in
+}
+get synopsis(){
+    return this._synopsis
+}
+get title(){
+    return this._title
+}
+get picture(){
+    return `assets/${this._picture}`
+}
+get thumbnails(){
+    return `assets/thumbnails/${this._picture}`
+}
 }
