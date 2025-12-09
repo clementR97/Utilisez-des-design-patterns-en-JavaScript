b114796f39884224055b0ff5e4f31600652f8586
clement Roland <clementroland52@gmail.com>
2025-12-09 18:24:41 +0000

    Implement caching in ProxyRatingSorter to optimize movie sorting

--- js/proxy/ProxyRatingSorter.js
+++ js/proxy/ProxyRatingSorter.js
@@ -3,6 +3,14 @@ class ProxyRatingSorter {
         this.cache = []
     }
 
-    async sorter() {
+    async sorter(movies,orderBy) {
+        const cachedResult = this.cache.find(elt => elt.key === orderBy)
+        if(cachedResult){
+            console.log('get from cache')
+            return cachedResult
+        }
+        const data = await RatingSorterApi.sorter(movies, orderBy)
+        this.cache.push(data)
+        return data
     }
 }
