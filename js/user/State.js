3f13b6982f3d77033b9adffb408438e13b0dd5db
clement Roland <clementroland52@gmail.com>
2025-12-19 00:22:56 +0000

    Implement user context and state management with initial state setup and user change functionality

--- js/user/State.js
+++ js/user/State.js
@@ -1,5 +1,19 @@
 class UserConnectedState {
+    constructor(){
+        this.isConnected = true
+        this.User = new User()
+    }
+    getUser(){
+        return new User()
+    }
 }
 
 class AnonymousUserState {
+    constructor(){
+        this.isConnected = true
+        this.User = new User()
+    }
+    getUser(){
+        return new User()
+    }
 }
